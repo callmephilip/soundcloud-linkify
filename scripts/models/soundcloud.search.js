ebe4e41c4a6efa17f83cf03b0743923166e8004f
callmephilip <philip.nuzhnyy@gmail.com>
2013-01-10 17:47:09 -0800

    adjust according to spec

--- scripts/models/soundcloud.search.js
+++ scripts/models/soundcloud.search.js
@@ -5,37 +5,31 @@
 
 define(["underscore", "models/soundcloud.base"], function(_,SouncloudModels){
 
-    var SoundCloudSearchCollection = SouncloudModels.Collection.extend({
-        
+    var ArtistSearchCollection = SouncloudModels.Collection.extend({
         parse: function(response) {
-            // tracks only, por favor
-            return _.map(_.filter(response.collection, function(r){
-                return r.kind === "track";
-            }),function(t){
-                return { 
-                    id : t.id,
-                    artwork_url : t.artwork_url,   
-                    title : t.title,
-                    user_avatar_url : t.user.avatar_url,
-                    user_username : t.user.username
-                };
-            });
+            return _.filter(response.collection, function(r){ return r.kind === "user"; });
         }
     });
 
 
     return {
-        search : function(query){
-            if(typeof query !== 'undefined'){
-                var c = this.newSearchCollection("/search?q=" + encodeURIComponent(query));
-                return c.fetch();
-            }
-        },
+        // search : function(query){
+        //     if(typeof query !== 'undefined'){
+        //         var c = this.newSearchCollection("/search?q=" + encodeURIComponent(query));
+        //         return c.fetch();
+        //     }
+        // },
 
-        newSearchCollection : function(url){
-            var c = new SoundCloudSearchCollection();
-            c.url = url;
-            return c;
+        findArtist : function(name){
+            var c = new ArtistSearchCollection();
+            c.url = "/search?q=" + encodeURIComponent(name);
+            return c.fetch();
         }
+
+        // newSearchCollection : function(url){
+        //     var c = new SoundCloudSearchCollection();
+        //     c.url = url;
+        //     return c;
+        // }
     };
 });
