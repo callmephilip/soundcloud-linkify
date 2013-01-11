2d0b636cb9e661e2725093e55491accf778f130b
callmephilip <philip.nuzhnyy@gmail.com>
2013-01-10 16:18:28 -0800

    implement initial artist highlghting

--- scripts/chrome/content.js
+++ scripts/chrome/content.js
@@ -20,26 +20,63 @@
         for(var i=0; i<targetSelectors.length; i++){
             batch = $(targetSelectors[i]);
             for(var j=0; j<batch.length; j++){
-                results.push(batch[j].innerHTML);
+                results.push(batch[j]);
             } 
         }
 
         return results;
     }
 
+    //convert plain text containing artist's name into something less plain
+    function formatArtist(name){
+        return '<span style="color:red;">'+ name +'</span>';
+    }
 
     //background is calling
     chrome.extension.onMessage.addListener(function(msg, _, sendResponse) {
         console.log("Got message from background page: ", msg);
     
+
         if(msg.what === 'inspect'){
             setTimeout(function(){
-                sendResponse(findContent());
-                console.log(findContent());
+                var content = findContent(), textContent = [];
+
+                for(var i=0; i<content.length; i++){
+                    textContent.push(content[i].innerHTML);
+                }
+
+                sendResponse(textContent);
+                console.log(content);
             }, 5000);
+
+            return true;
         }
 
-        return true;
+        if(msg.what === "artists"){
+
+            function htmlEscape(str) {
+                return String(str)
+                        .replace(/&/g, '&amp;')
+                        .replace(/"/g, '&quot;')
+                        .replace(/'/g, '&#39;')
+                        .replace(/</g, '&lt;')
+                        .replace(/>/g, '&gt;');
+            }
+
+            console.log("got artists from the background", msg.data);
+
+            var sections = findContent(), artists = msg.data;
+
+            for(var i=0; i<sections.length; i++){
+                var html = sections[i].innerHTML;
+                for(var j=0; j<artists.length; j++){
+                    html = html.replace(htmlEscape(artists[j]), formatArtist(artists[j]));
+                }
+
+                sections[i].innerHTML = html;
+            } 
+        }
+
     });
 
     // get the background script going
