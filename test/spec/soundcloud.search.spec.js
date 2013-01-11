13c2bbf7bebe0da905a6bfc6205cd16af8063b76
callmephilip <philip.nuzhnyy@gmail.com>
2013-01-10 17:46:42 -0800

    add spec for findArtist

--- test/spec/soundcloud.search.spec.js
+++ test/spec/soundcloud.search.spec.js
@@ -1,4 +1,4 @@
-define(["models/soundcloud.search"], function(Search){
+define(["models/soundcloud.search","soundcloud"], function(Search,SC){
     describe("Soundcloud Search",function(){
         
         describe("module", function(){
@@ -9,6 +9,30 @@ define(["models/soundcloud.search"], function(Search){
 
             it("exports findArtist", function(){
                 expect(Search).to.respondTo('findArtist');
+            });
+
+        });
+
+        describe("findArtist", function(){
+
+            it("hits SC's search with a query", function(done){
+                
+                var searchTerm = 'High Contrast';
+
+                sinon.stub(SC,'get',function(url){
+                    expect(url).to.equal('/search?q=' + encodeURIComponent(searchTerm));
+                    done();
+                });
+
+                Search.findArtist(searchTerm);
+            });
+
+            it("returns one artist if she's on soundcloud", function(){
+                //TODO
+            });
+
+            it("returns nothing if the artist is NOT on soundcloud", function(){
+                //TODO
             });
 
         });
