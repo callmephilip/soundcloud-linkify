0ebbef40ab578c2b6cca03af8dea139d45538279
callmephilip <philip.nuzhnyy@gmail.com>
2013-01-15 10:45:38 -0800

    integrate page caching

--- scripts/chrome/background.js
+++ scripts/chrome/background.js
@@ -20,102 +20,119 @@ require.config({
   }
 });
  
-require(['jquery','parser','decorator','verifier', 'soundcloud'], function($,Parser,Decorator,Verifier,SC) {
+require(['jquery','underscore','parser','decorator','verifier', 'soundcloud', 'models/page.cache'], 
 
-    function testing(){
-        return $("#mocha").length !== 0;
-    }
+    function($,_,Parser,Decorator,Verifier,SC,PageCache) {
 
-    //schedule tab inspection
-    function inspect(tab){
-        chrome.tabs.sendMessage(tab.id, { what : "inspect" });
-    }
-
-
-    function run(tab){
-        if(typeof tab !== 'undefined'){
-            
-            console.log("running with", tab);    
-
-            var context = {
-                tab : tab,
-                inspectedUrl : null
-            };
-
-
-            var urls = [
-                tab.url
-            ];
-
-            //keep an eye on the current tab to monitor navigation around the site
-            chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-                if(tab.id !== context.tab.id){ return; }
-             
-                if(typeof changeInfo.url !== 'undefined'){
-                    if(urls.indexOf(changeInfo.url) === -1){
-                        urls.push(changeInfo.url);
-                    }
-                }
-
-                if((changeInfo.status === 'complete') && (typeof tab.favIconUrl !== 'undefined')){
-                    // url has changed and the content is loaded
-                    if(urls.indexOf(tab.url) !== -1){
-                        urls.splice(urls.indexOf(tab.url),1);
-                        console.log("current tab updated",tab);
-                        inspect(tab);                        
-                    }
-                }
-            });
+        function testing(){
+            return $("#mocha").length !== 0;
         }
-    }
 
-    if(!testing()){
+        //schedule tab inspection
+        function inspect(tab){
+            chrome.tabs.sendMessage(tab.id, { what : "inspect" });
+        }
 
-        function getSoundCloudSettings(){
-            return { //production settings
-                client_id: "75183d3e642d5c008a5fa0d33a6e7da5",
-                redirect_uri: "http://callmephilip.github.com/stacks/soundcloud.callback.html"
+
+        function run(tab){
+            if(typeof tab !== 'undefined'){
+                
+                console.log("running with", tab);    
+
+                var context = {
+                    tab : tab,
+                    inspectedUrl : null
+                };
+
+
+                var urls = [
+                    tab.url
+                ];
+
+                //keep an eye on the current tab to monitor navigation around the site
+                chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+                    if(tab.id !== context.tab.id){ return; }
+                 
+                    if(typeof changeInfo.url !== 'undefined'){
+                        if(urls.indexOf(changeInfo.url) === -1){
+                            urls.push(changeInfo.url);
+                        }
+                    }
+
+                    if((changeInfo.status === 'complete') && (typeof tab.favIconUrl !== 'undefined')){
+                        // url has changed and the content is loaded
+                        if(urls.indexOf(tab.url) !== -1){
+                            urls.splice(urls.indexOf(tab.url),1);
+                            console.log("current tab updated",tab);
+                            inspect(tab);                        
+                        }
+                    }
+                });
             }
         }
 
-        // get the soundcloud goodness ready go
-        SC.initialize(getSoundCloudSettings());
+        if(!testing()){
 
-        // content script pings background asking it to start the app
-        chrome.extension.onRequest.addListener(function(request, sender, sendResponse){
-            sendResponse({});
-            chrome.tabs.getSelected(null,function(t){
-                run(t);
+            function getSoundCloudSettings(){
+                return { //production settings
+                    client_id: "75183d3e642d5c008a5fa0d33a6e7da5",
+                    redirect_uri: "http://callmephilip.github.com/stacks/soundcloud.callback.html"
+                }
+            }
+
+            // get the soundcloud goodness ready go
+            SC.initialize(getSoundCloudSettings());
+
+            // content script pings background asking it to start the app
+            chrome.extension.onRequest.addListener(function(request, sender, sendResponse){
+                sendResponse({});
+                chrome.tabs.getSelected(null,function(t){
+                    run(t);
+                });
             });
-        });
 
-        //content script is talking back
-        chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+            //content script is talking back
+            chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
                 
-            // content script sends over html to decorate
-            if(request.what === "decorate"){
-                console.log("BG: got decoration request",request);
-                
-                var artists = Parser.parse(request.data);
-
-                console.log("got content", request.data);
-                console.log("artists identified", artists);
-
-                //check every single identified artist against SC to get additional data
-
-                Verifier.verify(artists, function(verifiedArtists){
-                    console.log("got verified artists", verifiedArtists);
-
-                    if(verifiedArtists.length !== 0){
+                function notify(content, needsVisual){
+                    sendResponse(content);
+                    if(needsVisual){
                         chrome.pageAction.show(sender.tab.id);
                     }
+                }
 
-                    //update page content and send it back
-                    sendResponse(Decorator.decorate(request.data, verifiedArtists));
-                });
+                // content script sends over html to decorate
+                if(request.what === "decorate"){
+                    console.log("BG: got decoration request",request);
+                    
+                    //check if current page is in the cache
+                    if(PageCache.get(sender.tab.url)){
+                        notify(PageCache.get(sender.tab.url), true);
+                        return;
+                    }
 
-                return true;
-            }
-        });
+
+                    var artists = Parser.parse(request.data);
+
+                    console.log("got content", request.data);
+                    console.log("artists identified", artists);
+
+                    //check every single identified artist against SC to get additional data
+
+                    Verifier.verify(artists, function(verifiedArtists){
+                        console.log("got verified artists", verifiedArtists);
+
+                        var decoration = Decorator.decorate(request.data, verifiedArtists); 
+
+                        if(verifiedArtists.length !== 0){
+                            PageCache.set(sender.tab.url, decoration);
+                        }
+
+                        notify(decoration, verifiedArtists.length !== 0);
+                    });
+
+                    return true;
+                }
+            });
     }
 });
